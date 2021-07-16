fd4acec782e485ba35797f7482ba6ee7e61251ea
hodong <lhdong@mz.co.kr>
2021-07-16 10:34:19 +0900

    some change

--- lib/mzc-cdk-stack.ts
+++ lib/mzc-cdk-stack.ts
@@ -1,8 +1,17 @@
 import * as cdk from '@aws-cdk/core';
+import * as ecr from '@aws-cdk/aws-ecr';
+import * as eks from '@aws-cdk/aws-eks';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+
 
 export class MzcCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const ecrRepo = new ecr.Repository(this, 'EcrRepo');
+
 
     // The code that defines your stack goes here
   }
